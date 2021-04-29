548c4f27325aa7e25d2b446fb04e64014e4ca46d
asus18019 <72703431+asus18019@users.noreply.github.com>
2021-04-29 18:59:36 +0300

    added users

--- pages/fishes/index.js
+++ pages/fishes/index.js
@@ -1,13 +1,35 @@
 import Head from "next/head";
+import Link from "next/link";
+import styles from "../../styles/fish.module.css"
 
-const Cars = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    return{
+        props: {
+            users: data
+        }
+    }
+}
+
+const Cars = ({users}) => {
     return (
         <>
             <Head>
                 <title>Fishes | All</title>
             </Head>
             <div>
-                <h1>All fishes</h1>
+                <h1>All users</h1>
+                {
+                    users.map(user =>
+                        <Link href={'/fishes/' + user.id} key={user.id}>
+                            <a className={styles.single}>
+                                <h3>{ user.name }</h3>
+                            </a>
+                        </Link>
+                    )
+                }
             </div>
         </>
     );
